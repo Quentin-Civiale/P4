4212d38fc5a7a31c2fe735841079706cbe7304d0
Quentin Civiale <quentin.civiale@gmail.com>
2018-06-25 18:42:06 +0200

    Mise en place d'un service "Prix" pour calculer les différents tarifs

---
+++ src/Louvre/GeneralBundle/Services/Price.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Louvre\GeneralBundle\Services;
+
+use Louvre\GeneralBundle\Entity\Booking;
+use Louvre\GeneralBundle\Entity\Ticket;
+
+class Price
+{
+
+    const REDUCED_PRICE = 10;
+    const SENIOR_PRICE = 12;
+    const NORMAL_PRICE = 16;
+    const CHILD_PRICE = 8;
+    const HALF_PRICE = 1/2;
+    const FREE_PRICE = 0;
+
+}
